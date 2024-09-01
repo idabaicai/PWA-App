50e193795d9faa60e23a9aa23c14d61fadababa8
idabaicai <icandebug@gmail.com>
2024-09-01 10:21:20 +0800

    serving

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 // 缓存路径
-const urlsToCache = ["/app.js", "/index.html", "/styles/index.css"];
+const urlsToCache = ["/manifest.json", "/app.js", "/index.html", "/styles/index.css", "/imgs/photo.webp"];
 const cacheName = "pwa-assets";
 self.addEventListener("install", async (e) => {
   console.log("installed", e);
@@ -23,7 +23,62 @@ self.addEventListener("install", async (e) => {
 self.addEventListener("fetch", (e) => {
   const res = "hello pwa";
   console.log("fetch", e.request.url);
-  // e.respondWith(res);
+  const simpleResponse = new Response("Hello PWA");
+  const options = {
+    status: 200,
+    header: {
+      "Content-Type": "text/html",
+    },
+  };
+  const htmlResponse = new Response("<b>HTML</b> content", options);
+
+  // Global search on all caches in the current origin
+  // caches.match(e.request.url).then((response) => {
+  //   // console.log(response ? response : "It's not in the cache");
+  //   console.log("response :>> ", response);
+  // });
+
+  // Cache-specific search
+  // caches.open("pwa-assets").then((cache) => {
+  //   cache.match(e.request.url).then((response) => {
+  //     console.log(response ? response : "It's not in the cache");
+  //   });
+  // });
+
+  // cache first
+  // e.respondWith(
+  //   caches.match(e.request)
+  //     .then((cacheResponse) => {
+  //       return cacheResponse || fetch(e.request)
+  //     })
+  // )
+
+  // network first
+  // e.respondWith(
+  //   caches.match(e.request)
+  //     .catch((err) => {
+  //       return caches.match(e.request)
+  //     })
+  // )
+
+  // stale while revalidate
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      const networkFetch = fetch(e.request).then((response) => {
+        // update the cache with a clone of the network response
+        const responseClone = response.clone();
+        caches.open(cacheName).then((cache) => {
+          console.log("cache :>> ", cache);
+          cache.put(e.request, responseClone);
+        });
+        return response;
+      }).catch(reason => {
+        console.log('ServiceWorker fetch failed:', reason);
+      })
+      // prioritize cached response over network
+      return cachedResponse || networkFetch;
+    })
+  );
 });
 
 // 清除旧缓存
@@ -31,10 +86,10 @@ self.addEventListener("activate", async (e) => {
   console.log("activated", e);
   const keys = await caches.keys();
   keys.forEach((key) => {
-    if(key !== cacheName) {
+    if (key !== cacheName) {
       caches.delete(key);
     }
-  })
+  });
   // 激活后获取控制权
-  await self.clients.claim()
+  await self.clients.claim();
 });
